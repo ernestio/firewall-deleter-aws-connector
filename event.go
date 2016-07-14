35e5b76d70602fa75aa47705e24c45064aa7e294
Tom Bevan <tom.bevan@r3labs.io>
2016-07-14 17:04:08 +0100

    addded additional fields

--- event.go
+++ event.go
@@ -32,7 +32,9 @@ type rule struct {
 
 // Event stores the firewall data
 type Event struct {
-	ID                    string `json:"id"`
+	UUID                  string `json:"_uuid"`
+	BatchID               string `json:"_batch_id"`
+	ProviderType          string `json:"_type"`
 	DatacenterVPCID       string `json:"datacenter_vpc_id"`
 	DatacenterRegion      string `json:"datacenter_region"`
 	DatacenterAccessKey   string `json:"datacenter_access_key"`
